1e0d5ade88b0ce27e3dcb350ef8a18ac00bd7c35
CKirbs <ckggithub@gmail.com>
2023-07-17 21:38:55 +0800

    Made New Lines Possible, Add Auto Grow To Text Area Of /Create

--- scripts/Functions.js
+++ scripts/Functions.js
@@ -1,4 +1,9 @@
 exports.shorten = (desc, charAmount) => {
+  let descLines = desc.split(`\r\n`);
+  if (descLines.length > 3) {
+    descLines = [descLines[0], descLines[1], descLines[2]];
+    return descLines.join("\r\n") + "...";
+  }
   if (desc.length > charAmount) {
     return desc.slice(0, charAmount) + "...";
   }
