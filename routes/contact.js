358cbe3e58da09befa2bbad56916fd045a591a5e
CKirbs <ckggithub@gmail.com>
2023-07-17 14:36:25 +0800

    Added Create Design And Form

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/contact", (req, res) => {
-  res.render("contact");
+  res.render("contact", {path: "/contact"});
 });
 
 module.exports = {
--- routes/create.js
+++ routes/create.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/create", (req, res) => {
-  res.render("create");
+  res.render("create", {path: "/create"});
 });
 
 router.post("/create", (req, res) => {
